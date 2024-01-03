a9b0b2224ef8810de3e1872186a228ef24d24a02
Iven Han <iven.han@gmail.com>
2024-01-02 16:54:13 -0800

    Adding Firebase Hosting

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to My Website</h1>
+      <p>This is a static homepage created with React.</p>
+      <p>
+        After going through the wizard you are ready to do what about serving the site locally? After all you know what your site looks and works the way you want to deploying to production. To do that we can set up an Emulator Suite, which can run a whole number of Firebase locally. In this case though, we'll just set it upload hosting.
+      </p>
+
+      <pre>
+
+      </pre>
+    </div>
+  );
+};
+
+export default HomePage;
+
+
